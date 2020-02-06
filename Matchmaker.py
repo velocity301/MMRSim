02720687d423bcd2635a773f3689b1f4d853ea27
velocity301 <velocity300@gmail.com>
2020-02-05 18:39:43 -0800

    Update Matchmaker.py

--- Matchmaker.py
+++ Matchmaker.py
@@ -1,14 +1,16 @@
 # Matchmaker.py
 '''
-End goal is to be able to simulate the effects of game length, playerbase size, 
-smurfing, and other factors on matchmaking both on the average player and either 
-end of the skill spectrum.  
+End goal is to be able to simulate the effects of game length, playerbase size,
+smurfing, and other factors on matchmaking both on the average player and either
+end of the skill spectrum.
 '''
+import time
 import random
-playerbase = 10
-
+# Using numbers higher than a million will make it take quite a while
+playerbase = 1000
 players = []
-sortedPlayersMMRValues = []
+
+start_time = time.time()
 
 
 class Player:
@@ -44,27 +46,59 @@ class Player:
         # For later when we simulate actual games happenign
         self.status = "queueing"  # alternate values will be "playing" and "offline"
 
-    # The eq and lt definitions are so that we can sort our player objects by MMR later.
+    # These definitions are so that we can sort our player objects by MMR later.
     # This allows us to use the players.sort() and sorted(players) functions
     def __eq__(self, other):
         return self.MMR == other.MMR
 
     def __lt__(self, other):
         return self.MMR < other.MMR
+
+    def __gt__(self, other):
+        return self.MMR > other.MMR
+
+    def __le__(self, other):
+        return self.MMR <= other.MMR
+
+    def __ge__(self, other):
+        return self.MMR >= other.MMR
     # defines how the object should be displayed when represented anywhere.
     # without this when we would try to print our objects, we would get memory addresses
+    # it is currently set to display the player's MMR followed by the roles they play.
 
     def __repr__(self):
         return str(self.MMR) + " " + str(self.roles)
 
-    '''    
-    other things to consider adding to the Player Class: 
-    	-having MMR and actual skill be different values so we can have win probabilities of smurfs
-    	-having preferred hours of play or number of hours they're willing to play in a day
-	'''
+    '''
+    other things to consider adding to the Player Class:
+        -having MMR and actual skill be different values so we can have win probabilities of smurfs
+        -having preferred hours of play or number of hours they're willing to play in a day
+    '''
+
+
+# def qsort(inlist):
+#     if inlist == []:
+#         return []
+#     else:
+#         pivot = inlist[0]
+#         lesser = qsort([x for x in inlist[1:] if x < pivot])
+#         greater = qsort([x for x in inlist[1:] if x >= pivot])
+#         return lesser + [pivot] + greater
 
 
 # player generation
 for i in range(playerbase):
     players.append(Player())
-print(sorted(players))
+time1 = time.time()
+
+time2 = time.time()
+players.sort()
+print(players)
+print("--- %s seconds ---" % (time1 - start_time))
+print("--- %s seconds ---" % (time.time() - time2))
+time3 = time.time()
+# newStart = time.time() - start_time
+
+# print(qsort(players))
+
+# print("--- %s seconds ---" % (time.time() - newStart))
